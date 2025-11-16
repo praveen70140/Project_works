7e22d0460b7c6911486209005701be350cc2c85c
SuyashC1000 <107310104+SuyashC1000@users.noreply.github.com>
2025-11-16 22:48:58 +0530

    chore: add heroui components

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,14 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Gabarito } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const gabarito = Gabarito({
+  variable: "--font-gabarito",
   subsets: ["latin"],
 });
 
@@ -24,10 +20,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${gabarito.className} antialiased`}>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
